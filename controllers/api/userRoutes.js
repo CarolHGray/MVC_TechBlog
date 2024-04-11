d36f4bbe7440c5dff075b5de3bdecb89e9d4827d
CarolHGray <fivegrays@yahoo.com>
2024-04-11 09:35:56 -0400

    added a user routes file to the api folder

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,41 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+// Posts new user email, username, and password to database
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// When user logs in as an existing user then this route validates user credentials and logs user in if a match is found in the database
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      console.log("no user found");
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      console.log("no password match");
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
